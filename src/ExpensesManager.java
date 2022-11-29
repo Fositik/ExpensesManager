601d50feb5c695de8c34c7ce0d83cb01e7b2e61d
Fositik <fositik@yandex.ru>
2022-11-29 23:04:20 +0300

    Solved

--- src/ExpensesManager.java
+++ src/ExpensesManager.java
@@ -3,37 +3,33 @@ import java.util.HashMap;
 
 public class ExpensesManager {
     HashMap<String, ArrayList<Double>> expensesByCategories;
-    ArrayList<Double> expenses = new ArrayList<>();
+
     // Замените на таблицу с именем expensesByCategories
 
     ExpensesManager() {
         expensesByCategories = new HashMap<>();
         // Создайте таблицу
     }
-
     // Добавьте в метод ещё один параметр — category
     double saveExpense(double moneyBeforeSalary, double expense, String category) {
         moneyBeforeSalary = moneyBeforeSalary - expense;
         System.out.println("Значение сохранено! Ваш текущий баланс в рублях: " + moneyBeforeSalary);
-
         // Замените на работу с таблицей
         if (expensesByCategories.containsKey(category)) {  // Проверьте наличие категории
-            expensesByCategories.get(category);// Получите список трат в этой категории
-            expenses.add(expense);
-            expensesByCategories.put(category, expenses);
-            // Добавьте трату
-        } else {
-            ArrayList<Double> newExpenses = new ArrayList<>();
-            newExpenses.add(expense);// Создайте новый список трат и добавьте в него сумму
-            expensesByCategories.put(category, newExpenses); // Сохраните категорию и новый список трат в хеш-таблицу
-        }
-
-        if (moneyBeforeSalary < 1000) {
-            System.out.println("На вашем счету осталось совсем немного. Стоит начать экономить!");
+            if (expensesByCategories.containsKey(category)) {
+                ArrayList<Double> expenses = expensesByCategories.get(category);
+                expenses.add(expense);
+            } else {
+                ArrayList<Double> newExpenses = new ArrayList<>();
+                newExpenses.add(expense);// Создайте новый список трат и добавьте в него сумму
+                expensesByCategories.put(category, newExpenses); // Сохраните категорию и новый список трат в хеш-таблицу
+            }
+            if (moneyBeforeSalary < 1000) {
+                System.out.println("На вашем счету осталось совсем немного. Стоит начать экономить!");
+            }
         }
         return moneyBeforeSalary;
     }
-
 
     void printAllExpensesByCategories() {
         // Замените логику для работы с таблицами
